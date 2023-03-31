7f7a258e1d0af79327c09b608d8d4545a21d5e61
Mert Yilmaz <82391850+lazzante@users.noreply.github.com>
2023-03-31 11:52:17 +0300

    Update SecurityConfig.java

--- src/main/java/laurentiuspilca/managing_users_lesson_01/config/SecurityConfig.java
+++ src/main/java/laurentiuspilca/managing_users_lesson_01/config/SecurityConfig.java
@@ -6,15 +6,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.httpBasic()
                 .and().authorizeHttpRequests()
+                .requestMatchers("/demo").hasAuthority("ADMIN")
                 .anyRequest().authenticated()
-                .anyRequest().hasAuthority("READ")
                 .and().build();
 
     }
