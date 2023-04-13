ed93cab66c8f7fa4f95b9526807e2c005154a68e
mrtylmz <mrtylmz5306@gmail.com>
2023-04-13 12:50:31 +0300

    CustomFormLogin

--- src/main/java/CustomFormLogin/config/SecurityConfig.java
+++ src/main/java/CustomFormLogin/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -54,6 +55,7 @@ public class SecurityConfig {
                 .and().formLogin()
                 .and().build(); */
     }
+
 
 
 
